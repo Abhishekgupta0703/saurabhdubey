2ca8ef8127d0ef2e02698ffb85d1e0995f5baf22
Abhishekgupta0703 <ak19250000@gmail.com>
2026-07-01 14:01:48 +0530

    m

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     server: { entry: "server" },
   },
   nitro: {
-    preset: "netlify",
+    preset: "vercel",
     output: {
       dir: "dist",
       publicDir: "dist/public",
